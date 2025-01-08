f68021bb10fb9e3e6ee5168695638c0094dc8769
Lucas Hardt <Luc1412.lh@gmail.com>
2025-01-08 14:07:07 +0100

    Move PushRequestListener to main view

--- lib/views/tokens_view/tokens_view.dart
+++ lib/views/tokens_view/tokens_view.dart
@@ -7,7 +7,6 @@ import 'package:edumfa_authenticator/views/tokens_view/tokens_view_widgets/main_
 import 'package:edumfa_authenticator/views/tokens_view/tokens_view_widgets/tokens_list.dart';
 import 'package:edumfa_authenticator/views/tokens_view/tokens_view_widgets/tokens_list_filtered.dart';
 import 'package:edumfa_authenticator/views/view_interface.dart';
-import 'package:edumfa_authenticator/widgets/push_request_listener.dart';
 import 'package:edumfa_authenticator/widgets/status_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -30,43 +29,41 @@ class _TokensViewState extends ConsumerState<TokensView> {
   @override
   Widget build(BuildContext context) {
     final hasFilter = ref.watch(tokenFilterProvider) != null;
-    return PushRequestListener(
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        floatingActionButton: const QrScannerButton(),
-        body: ExpandableAppBar(
-          startExpand: hasFilter,
-          appBar: AppBar(
-              titleSpacing: 6,
-              centerTitle: true,
-              actions: [
-                hasFilter
-                    ? AppBarItem(
-                  onPressed: () {
-                    ref.read(tokenFilterProvider.notifier).state = null;
-                  },
-                  icon: const Icon(Icons.close),
-                )
-                    : AppBarItem(
-                  onPressed: () {
-                    ref.read(tokenFilterProvider.notifier).state =
-                        TokenFilter(
-                          searchQuery: '',
-                        );
-                  },
-                  icon: const Icon(Icons.search),
-                ),
-              ]),
-          body: ConnectivityListener(
-            child: StatusBar(
-              child: !hasFilter
-                  ? Stack(
-                children: [
-                  TokensList(nestedScrollViewKey: globalKey),
-                ],
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      floatingActionButton: const QrScannerButton(),
+      body: ExpandableAppBar(
+        startExpand: hasFilter,
+        appBar: AppBar(
+            titleSpacing: 6,
+            centerTitle: true,
+            actions: [
+              hasFilter
+                  ? AppBarItem(
+                onPressed: () {
+                  ref.read(tokenFilterProvider.notifier).state = null;
+                },
+                icon: const Icon(Icons.close),
               )
-                  : const TokensListFiltered(),
-            ),
+                  : AppBarItem(
+                onPressed: () {
+                  ref.read(tokenFilterProvider.notifier).state =
+                      TokenFilter(
+                        searchQuery: '',
+                      );
+                },
+                icon: const Icon(Icons.search),
+              ),
+            ]),
+        body: ConnectivityListener(
+          child: StatusBar(
+            child: !hasFilter
+                ? Stack(
+              children: [
+                TokensList(nestedScrollViewKey: globalKey),
+              ],
+            )
+                : const TokensListFiltered(),
           ),
         ),
       ),
