f37107ee6e4bba965557e858864b53cf630e6051
Lucas Hardt <Luc1412.lh@gmail.com>
2025-01-22 16:16:52 +0100

    restructure action button for search

--- lib/views/tokens_view/tokens_view.dart
+++ lib/views/tokens_view/tokens_view.dart
@@ -2,7 +2,6 @@ import 'package:edumfa_authenticator/generated/l10n.dart';
 import 'package:edumfa_authenticator/model/states/token_filter.dart';
 import 'package:edumfa_authenticator/utils/riverpod_providers.dart';
 import 'package:edumfa_authenticator/views/tokens_view/tokens_view_widgets/add_token_sheet.dart';
-import 'package:edumfa_authenticator/views/tokens_view/tokens_view_widgets/app_bar_item.dart';
 import 'package:edumfa_authenticator/views/tokens_view/tokens_view_widgets/connectivity_listener.dart';
 import 'package:edumfa_authenticator/views/tokens_view/tokens_view_widgets/expandable_appbar.dart';
 import 'package:edumfa_authenticator/views/tokens_view/tokens_view_widgets/tokens_list.dart';
@@ -48,23 +47,22 @@ class _TokensViewState extends ConsumerState<TokensView> {
             titleSpacing: 6,
             centerTitle: true,
             actions: [
-              hasFilter
-                  ? AppBarItem(
-                onPressed: () {
-                  ref.read(tokenFilterProvider.notifier).state = null;
-                },
-                icon: const Icon(Icons.close),
-              )
-                  : AppBarItem(
-                onPressed: () {
-                  ref.read(tokenFilterProvider.notifier).state =
-                      TokenFilter(
-                        searchQuery: '',
-                      );
-                },
-                icon: const Icon(Icons.search),
+              IconButton(
+                padding: const EdgeInsets.all(0),
+                splashRadius: 20,
+                onPressed: () => ref.read(tokenFilterProvider.notifier).state =
+                hasFilter
+                    ? null
+                    : TokenFilter(searchQuery: ''),
+                color: Theme.of(context).navigationBarTheme.iconTheme?.resolve({})?.color,
+                icon: SizedBox(
+                  height: 24,
+                  width: 24,
+                  child: FittedBox(child: Icon(hasFilter ? Icons.close : Icons.search)),
+                ),
               ),
-            ]),
+            ],
+        ),
         body: ConnectivityListener(
           child: StatusBar(
             child: !hasFilter
