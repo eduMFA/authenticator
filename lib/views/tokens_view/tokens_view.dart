c37533b67e5f854aaf693ea934d6fbd650061958
Lucas Hardt <Luc1412.lh@gmail.com>
2025-03-06 23:46:41 +0100

    update tokens view for cupertino

--- lib/views/tokens_view/tokens_view.dart
+++ lib/views/tokens_view/tokens_view.dart
@@ -8,7 +8,9 @@ import 'package:edumfa_authenticator/views/tokens_view/tokens_view_widgets/token
 import 'package:edumfa_authenticator/views/view_interface.dart';
 import 'package:edumfa_authenticator/widgets/conditional_floating_action_button.dart';
 import 'package:edumfa_authenticator/widgets/status_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:haptic_feedback/haptic_feedback.dart';
 
@@ -28,21 +30,39 @@ class TokensViewState extends ConsumerState<TokensView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      floatingActionButton: !isTablet(context)
-          ? ConditionalFloatingActionButton(
-            isExtended: ref.watch(tokenProvider).tokens.isEmpty,
-            label: S.of(context).addToken,
-            icon: Icons.add,
+    return PlatformScaffold(
+      material: (_, __) => MaterialScaffoldData(
+        resizeToAvoidBottomInset: false,
+        floatingActionButton: !isTablet(context)
+            ? ConditionalFloatingActionButton(
+          isExtended: ref.watch(tokenProvider).tokens.isEmpty,
+          label: S.of(context).addToken,
+          icon: Icons.add,
+          onPressed: () async => await showAddTokenSheet(context),
+        ) : null,
+        appBar: const PreferredSize(
+          preferredSize: Size.fromHeight(70),
+          child: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+              child: TokenSearchBar(),
+            ),
+          ),
+        ),
+      ),
+      appBar: PlatformAppBar(
+        cupertino: (context, platform) => CupertinoNavigationBarData(
+          title: Text(S.of(context).tokens),
+          trailing: CupertinoButton(
+            child: const Icon(CupertinoIcons.add),
             onPressed: () async => await showAddTokenSheet(context),
-          ) : null,
-      appBar: const PreferredSize(
-        preferredSize: Size.fromHeight(70),
-        child: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.only(left: 20, right: 20, top: 10),
-            child: TokenSearchBar(),
+          ),
+          bottom: const PreferredSize(
+              preferredSize: Size.fromHeight(35.0 + 8 * 2), 
+              child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+                  child: TokenSearchBar(),
+              )
           ),
         ),
       ),
